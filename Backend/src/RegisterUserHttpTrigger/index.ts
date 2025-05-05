d50c91041adbb3723fb2f6de1f539d6f13284d0f
Thomas Covens <thomas.covens@student.ucll.be>
2025-05-05 18:50:10 +0200

    p

--- Backend/src/RegisterUserHttpTrigger/index.ts
+++ Backend/src/RegisterUserHttpTrigger/index.ts
@@ -6,30 +6,17 @@ import {
 } from "@azure/functions";
 import { UserService } from "../service/user.service";
 import { unauthenticatedRouteWrapper } from "../helpers/function-wrapper";
-
-// The registerUser function is used to register a new user.
-export async function registerUser(
-  req: HttpRequest,
-  context: Context,
-): Promise<void> {
+const registerUser: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
   await unauthenticatedRouteWrapper(async () => {
     context.log("HTTP trigger function processed a request.");
-    // Validate the request body
-    if (
-      !req.body ||
-      !req.body.email ||
-      !req.body.password ||
-      !req.body.username
-    ) {
+    if (!req.body || !req.body.email || !req.body.password || !req.body.username) {
       throw new Error("Please provide an email and password to register.");
     }
 
     const { username, email, password } = req.body;
 
-    // Add the new user
     await UserService.getInstance().addUser(username, email, password);
 
-    // Return a successful response
     context.res = {
       status: 201,
       body: { email, username },
@@ -37,5 +24,7 @@ export async function registerUser(
         "Content-Type": "application/json",
       },
     };
-  }, context); // Pass both req and context for the wrapper function
-}
+  }, context);
+};
+
+export default registerUser;
