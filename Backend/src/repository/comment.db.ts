6eb5ef844ab0a46ee4ff38230c1003f32a89b770
Thomas Covens <thomas.covens@student.ucll.be>
2025-05-08 08:02:18 +0200

    testing

--- Backend/src/repository/comment.db.ts
+++ Backend/src/repository/comment.db.ts
@@ -7,158 +7,169 @@ import { ThreadRepository } from "./thread.db";
 // Deze krijg je als je een object oproept vanuit de databank
 // Vergelijkbaar met een prisma object
 interface CosmosCommentDocument {
-    id: string,
-    content: string,
-    creationDate: Date,
-    user: string, // Dit is een string omdat ik een ID opsla ipv het volledig user Object
-    thread: string // Dit is een string omdat ik een ID opsla ipv het volledig thread Object
+  id: string;
+  content: string;
+  creationDate: Date;
+  user: string; // Dit is een string omdat ik een ID opsla ipv het volledig user Object
+  thread: string; // Dit is een string omdat ik een ID opsla ipv het volledig thread Object
 }
 
 export class CommentRepository {
-    private static instance: CommentRepository;
-    private readonly container: Container;
+  private static instance: CommentRepository;
+  private readonly container: Container;
 
-    constructor (container: Container) {
-        if (!container) {
-            throw new Error("Container Repository Cosmos Container Required");
-        }
-
-        this.container = container;
+  constructor(container: Container) {
+    if (!container) {
+      throw new Error("Container Repository Cosmos Container Required");
     }
-    // Met deze functie maak je een instantie aan van de container
-    // CommentRepository.getInstance() kan gebruikt worden om een instantie van de repository te maken in andere repositories.
-    static getInstance = async() => {
-        const key = process.env.COSMOS_KEY;
-        const endpoint = process.env.COSMOS_ENDPOINT;
-        const databaseName = process.env.COSMOS_DATABASE_NAME;
-        const containerName = "threads";
-        const partitionKeyPath = ["/partition"];
 
-        if (!key || !endpoint) {
-            throw new Error("Cosmos key and endpoint should be specified.")
-        }
+    this.container = container;
+  }
+  // Met deze functie maak je een instantie aan van de container
+  // CommentRepository.getInstance() kan gebruikt worden om een instantie van de repository te maken in andere repositories.
+  static getInstance = async () => {
+    const key = process.env.COSMOS_KEY;
+    const endpoint = process.env.COSMOS_ENDPOINT;
+    const databaseName = process.env.COSMOS_DATABASE_NAME;
+    const containerName = "threads";
+    const partitionKeyPath = ["/partition"];
 
-        const cosmosClient = new CosmosClient({key: key, endpoint: endpoint});
+    if (!key || !endpoint) {
+      throw new Error("Cosmos key and endpoint should be specified.");
+    }
 
-        const {database} = await cosmosClient.databases.createIfNotExists({id: databaseName});
-        const { container } = await database.containers.createIfNotExists({
-            id: containerName,
-            partitionKey: {
-                paths: partitionKeyPath
-            }
-        })
+    const cosmosClient = new CosmosClient({ key: key, endpoint: endpoint });
 
-        this.instance = new CommentRepository(container);
+    const { database } = await cosmosClient.databases.createIfNotExists({
+      id: databaseName,
+    });
+    const { container } = await database.containers.createIfNotExists({
+      id: containerName,
+      partitionKey: {
+        paths: partitionKeyPath,
+      },
+    });
 
-        return this.instance;
+    this.instance = new CommentRepository(container);
+
+    return this.instance;
+  };
+
+  // Deze functie gaat een Cosmos object naar een comment object veranderen
+  // Jij kan het vergelijken met de User.from() functie van bij prisma.
+  toComment = async (
+    commentDocument: CosmosCommentDocument,
+  ): Promise<Comment> => {
+    // Ik nest geen objecten tijdens de create functie:
+    // Ipv een volledig user object opnieuw op te slaan als ik een thread aanmaak, ga ik gewoon de threadId geven
+    // Ik heb de user en thread nodig dus ik maak een instantie van de beide repositories.
+    const userRepository = await UserRepository.getInstance();
+    const threadRepository = await ThreadRepository.getInstance();
+
+    // Hier fetch ik de user en thread door hun ID te geven
+    const user = await userRepository.findUserById(
+      parseInt(commentDocument.user),
+    );
+    const thread = await threadRepository.findThreadById(
+      commentDocument.thread,
+    );
+
+    if (!user) {
+      throw new Error(
+        "Cannot change commentDocument to Comment due to missing user",
+      );
+    }
+
+    if (!thread) {
+      throw new Error(
+        "Cannot change commentDocument to Comment due to missing thread",
+      );
+    }
+
+    return new Comment({
+      id: parseInt(commentDocument.id),
+      content: commentDocument.content,
+      creationDate: commentDocument.creationDate,
+      user: user,
+      thread: thread,
+    });
+  };
+
+  // Ik maak gebruik van een SQL query die alle comments uit de container pakt
+  // Dan fetch ik alle comments met de .fetchAll() functie
+  // Als laatste transformeer ik alle CosmosCommentObjecten naar normale comment objecten.
+  getAllComments = async (): Promise<Comment[]> => {
+    const query = {
+      query: "SELECT * FROM c",
     };
 
-    // Deze functie gaat een Cosmos object naar een comment object veranderen
-    // Jij kan het vergelijken met de User.from() functie van bij prisma.
-    toComment = async(commentDocument: CosmosCommentDocument): Promise<Comment> => {
-        // Ik nest geen objecten tijdens de create functie: 
-        // Ipv een volledig user object opnieuw op te slaan als ik een thread aanmaak, ga ik gewoon de threadId geven
-        // Ik heb de user en thread nodig dus ik maak een instantie van de beide repositories.
-        const userRepository = await UserRepository.getInstance();
-        const threadRepository = await ThreadRepository.getInstance();
-        
-        // Hier fetch ik de user en thread door hun ID te geven
-        const user = await userRepository.findUserById(parseInt(commentDocument.user));
-        const thread = await threadRepository.findThreadById(commentDocument.thread);
+    const { resources } = await this.container.items.query(query).fetchAll();
+    if (resources.length > 0) {
+      return Promise.all(resources.map((resource) => this.toComment(resource)));
+    } else {
+      throw new Error("Comment not found.");
+    }
+  };
+  // Query: Zoekt elke comment die matcht de gegeven ID
+  // De .fetchAll() geeft een lijst terug (Ookal is het maar 1 item).
+  // Als de lijst meer dan 0 items heeft, dan word te cosmosObject getransformeerd naar een comment object en wordt teruggegeven
+  findCommentById = async (id: string): Promise<Comment> => {
+    const query = {
+      query: "SELECT * FROM c WHERE c.id = @id",
+      parameters: [{ name: "@id", value: id }],
+    };
 
-        if (!user) {
-            throw new Error("Cannot change commentDocument to Comment due to missing user");
-        }
+    const { resources } = await this.container.items.query(query).fetchAll();
+    if (resources.length > 0) {
+      return this.toComment(resources[0]);
+    } else {
+      throw new Error("Comment not found.");
+    }
+  };
 
-        if (!thread) {
-            throw new Error("Cannot change commentDocument to Comment due to missing thread");
-        }
+  findCommentsByUserUsername = async (username: string): Promise<Comment[]> => {
+    // Wij hebben de userID opgeslagen ipv een volledig user object bij het aanmaken van een comment.
+    // Dat betekent dat wij de user eerst moeten fetchen met de username, dan kunnen wij zijn ID krijgen
+    // Hier maak ik een instantie aan van de userRepository
+    const userRepository = await UserRepository.getInstance();
+    const user = await userRepository.findUserByUsername(username);
 
-        return new Comment({
-            id: parseInt(commentDocument.id),
-            content: commentDocument.content,
-            creationDate: commentDocument.creationDate,
-            user: user,
-            thread: thread
-        })
+    if (!user) {
+      throw new Error("Could not find user.");
     }
 
-    // Ik maak gebruik van een SQL query die alle comments uit de container pakt
-    // Dan fetch ik alle comments met de .fetchAll() functie
-    // Als laatste transformeer ik alle CosmosCommentObjecten naar normale comment objecten.
-    getAllComments = async(): Promise<Comment[]> => {
-        const query = {
-            query: "SELECT * FROM c"
-        }
+    // Query fetch alle comments met de userID van de user
+    const query = {
+      query: "SELECT * FROM c WHERE c.user = @id",
+      parameters: [{ name: "@id", value: user.getId() ?? null }],
+    };
 
-        const {resources} = await this.container.items.query(query).fetchAll();
-        if (resources.length > 0) {
-            return Promise.all(resources.map((resource) => this.toComment(resource)));
-        } else {
-            throw new Error("Comment not found.")
-        }
+    const { resources } = await this.container.items.query(query).fetchAll();
+    if (resources.length > 0) {
+      return Promise.all(resources.map((resource) => this.toComment(resource)));
+    } else {
+      throw new Error("Comments not found.");
     }
-    // Query: Zoekt elke comment die matcht de gegeven ID
-    // De .fetchAll() geeft een lijst terug (Ookal is het maar 1 item).
-    // Als de lijst meer dan 0 items heeft, dan word te cosmosObject getransformeerd naar een comment object en wordt teruggegeven
-    findCommentById = async(id: string): Promise<Comment> => {
-            const query = {
-                query: "SELECT * FROM c WHERE c.id = @id",
-                parameters: [{ name: "@id", value: id}]
-            };
-    
-            const {resources} = await this.container.items.query(query).fetchAll();
-            if (resources.length > 0) {
-                return this.toComment(resources[0]);
-            } else {
-                throw new Error("Comment not found.")
-            }
-        }
+  };
 
-    findCommentsByUserUsername = async(username: string): Promise<Comment[]> => {
-        // Wij hebben de userID opgeslagen ipv een volledig user object bij het aanmaken van een comment.
-        // Dat betekent dat wij de user eerst moeten fetchen met de username, dan kunnen wij zijn ID krijgen
-        // Hier maak ik een instantie aan van de userRepository
-        const userRepository = await UserRepository.getInstance();
-        const user = await userRepository.findUserByUsername(username);
+  createComment = async (comment: Comment): Promise<Comment> => {
+    // Omdat er geen auto-increment is fetch ik alle comments en dan doe ik de lengte van deze lijst + 1.
+    const id = (await this.getAllComments()).length + 1;
 
-        if (!user) {
-            throw new Error("Could not find user.")
-        }
-        
+    // Bij user en thread geef ik de ID, niet de volledige object
+    // Dit zorgt er voor dat er referentie is naar de al bestaande objecten in db.
+    const result = await this.container.items.create({
+      id: id.toString(),
+      content: comment.getContent(),
+      creationDate: comment.getCreationDate(),
+      user: comment.getUser().getId(),
+      thread: comment.getThread().getId(),
+    });
 
-        // Query fetch alle comments met de userID van de user
-        const query = {
-            query: "SELECT * FROM c WHERE c.user = @id",
-            parameters: [{ name: "@id", value: user.getId() ?? null}]
-        };
-
-        const {resources} = await this.container.items.query(query).fetchAll();
-        if (resources.length > 0) {
-            return Promise.all(resources.map((resource) => this.toComment(resource)));
-        } else {
-            throw new Error("Comments not found.")
-        }
-    }   
-
-    createComment = async(comment: Comment): Promise<Comment> => {
-        // Omdat er geen auto-increment is fetch ik alle comments en dan doe ik de lengte van deze lijst + 1.
-        const id = (await this.getAllComments()).length + 1;
-
-        // Bij user en thread geef ik de ID, niet de volledige object
-        // Dit zorgt er voor dat er referentie is naar de al bestaande objecten in db.
-        const result = await this.container.items.create({
-            id: id.toString(),
-            content: comment.getContent(),
-            creationDate: comment.getCreationDate(),
-            user: comment.getUser().getId(),
-            thread: comment.getThread().getId()
-        })
-
-        if (result && result.statusCode == 200) {
-            return this.findCommentById(id.toString());
-        } else {
-            throw new Error("Could not create comment.")
-        }
+    if (result && result.statusCode == 200) {
+      return this.findCommentById(id.toString());
+    } else {
+      throw new Error("Could not create comment.");
     }
+  };
 }
--- Backend/src/service/user.service.ts
+++ Backend/src/service/user.service.ts
@@ -3,7 +3,6 @@ import { User } from "../domain/user";
 import { UserRepository } from "../repository/user.db";
 
 export class UserService {
-
   private static instance: UserService;
 
   static getInstance() {
@@ -17,31 +16,38 @@ export class UserService {
     return UserRepository.getInstance();
   }
 
-  async addUser(username:string, email: string, password: string) {
+  async addUser(username: string, email: string, password: string) {
     if (!email || email.length < 3) {
-      throw new Error('Email is invalid.');
+      throw new Error("Email is invalid.");
     }
 
     if (!password || password.length < 8 || password.length > 64) {
-      throw new Error('Password must be between 8 and 64 characters.');
+      throw new Error("Password must be between 8 and 64 characters.");
     }
 
     if (await (await this.getRepo()).userExists(email)) {
-      throw new Error('A user with this email address already exists.');
+      throw new Error("A user with this email address already exists.");
     }
 
     if (await (await this.getRepo()).userExists(username)) {
-      throw new Error('A user with this username already exists.');
+      throw new Error("A user with this username already exists.");
     }
 
     const hashedPassword = await hash(password);
-    const user = new User({username, email, password: hashedPassword, creationDate: new Date(), role: 'user', reputation: 'Beginner'});
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+      creationDate: new Date(),
+      role: "user",
+      reputation: "Beginner",
+    });
     return (await this.getRepo()).createUser(user);
   }
 
   async findUserByEmail(email: string) {
     if (!email) {
-      throw new Error('Email is invalid.');
+      throw new Error("Email is invalid.");
     }
 
     return (await this.getRepo()).findUserByEmail(email);
