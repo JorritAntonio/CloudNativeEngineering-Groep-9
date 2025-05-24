f2d68df30207717c5ebd9c4f468989c86023db2b
Thomas Covens <thomas.covens@student.ucll.be>
2025-05-24 16:58:05 +0200

    we probereb opniewu

--- upload-site.sh
+++ upload-site.sh
@@ -15,7 +15,7 @@ find "$local_folder" -type f | while read -r file_path; do
         relative_path=${file_path#$local_folder/}
 
         # Construct the Blob Storage URL for the file
-        blob_url="https://cnegroep9projectcos8b7d.z28.web.core.windows.net/$container_name/$relative_path?$sas_token"
+        blob_url="https://$storage_account.blob.core.windows.net/$container_name/$relative_path?$sas_token"
 
         # Set Content-Type based on file extension
         extension="${file_path##*.}"
