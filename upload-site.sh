2d197f0e5fa3366625ce582b51a7cdaafd70b594
Thomas Covens <thomas.covens@student.ucll.be>
2025-05-25 11:55:11 +0200

    fix link script

--- upload-site.sh
+++ upload-site.sh
@@ -8,6 +8,9 @@ sas_token="$AZURE_STORAGE_SAS_TOKEN"
 # Set the local folder path
 local_folder="frontend/out"
 
+# Run link fix script before upload
+node frontend/fix-link.js
+
 # Iterate over each file in the local folder and its subfolders
 find "$local_folder" -type f | while read -r file_path; do
     if [ -f "$file_path" ]; then
